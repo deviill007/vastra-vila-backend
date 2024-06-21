a82fa4c68c89df125b19a9c4245152e99b3919a8
Kavya Sharma <8kavyasharma@gmail.com>
2024-06-21 17:07:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `${process.env.API_BASE_URL}/${PORT}/images/${req.file.filename}`,
+    image_url: `https://vastra-vila.onrender.com/${PORT}/images/${req.file.filename}`,
   });
 });
 
